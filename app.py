86ea667a23b80cdaeabb5fe593176b086eb4d2bd
TheDark-15 <pretelalva39@gmail.com>
2026-07-03 18:46:28 -0500

    Update app.py

--- app.py
+++ app.py
@@ -48,6 +48,8 @@ class Customer(db.Model):
 
     email = db.Column(db.String(100), unique=True, nullable=False)
 
+    password = db.Column(db.String(255), nullable=False)
+
 class Purchase(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
